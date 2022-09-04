80cc4202b216866f1f4f97ff60463b69234997d5
HuGoxDP <sasha.maidanika@gmail.com>
2022-09-04 13:38:11 +0300

    add sort Age_Weight_Height

--- newClass/class/Program.cs
+++ newClass/class/Program.cs
@@ -15,16 +15,21 @@ namespace @class
             List<Human> listHuman = new List<Human>();
             Adress add1 = new Adress("Ukraine", "Kherson", "Perekopskaya", 3);
             Human one = new Human("Alesia", "Tantsiurenko", 18, 1.84, 60, false, Nation.Ukranian, add1);
-            Human two = new Human("Alex", "Ker", 18, 1.84, 100, true, Nation.Polish, new Adress("Polish", "Jahj", "Thgg", 5));
+            Human two = new Human("Alex", "Ker", 19, 1.84, 100, true, Nation.Polish, new Adress("Polish", "Jahj", "Thgg", 5));
+            Human three = new Human("Antony", "admant", 17, 1.86, 89, false, Nation.French, new Adress("French", "Oves", "Almat", 1));
+            Human four = new Human("Kiril", "Scroll", 23, 1.74, 75, true, Nation.Ukranian, new Adress("Ukranian", "Oda", "Thu", 4));
 
             listHuman list = new listHuman();
             list.add(one);
             list.add(two);
+            list.add(three);
+            list.add(four);
             Console.WriteLine("Выберите действие ");
             Console.WriteLine("1 - распечатать все объекты");
             Console.WriteLine("2 - найти объект по имени");
             Console.WriteLine("3 - сортировка");
-            Console.WriteLine("4 - удалить объект");
+            Console.WriteLine("4 - сортировка по ...");
+            Console.WriteLine("5 - удалить объект");
             int i = int.Parse(Console.ReadLine());
             switch (i)
             {
@@ -38,6 +43,25 @@ namespace @class
                     break;
                 case 3:
                     list.average_age();
+                    break;
+                case 4:
+                    Console.WriteLine(" 1 - Возраст \n 2 - вес \n 3 - рост");
+                    int ij = int.Parse(Console.ReadLine());
+                    switch (ij) {
+                        case 1:
+                        list.Age_weight_height_sort(1);
+                            break;
+                            case 2:
+                        list.Age_weight_height_sort(2);
+                            break;
+                        case 3:
+                        list.Age_weight_height_sort(3);
+                            break;
+                        default:
+                            Console.WriteLine("Вы ввели неверный номер!");
+                            break;
+                        }
+
                     break;
                 default:
                     Console.WriteLine("Вы ввели неверный номер!");
--- newClass/class/listHuman.cs
+++ newClass/class/listHuman.cs
@@ -46,10 +46,45 @@ namespace @class
             }
 
         }
-        /*public void sort()
+        public void Age_weight_height_sort(int choise)
         {
-        
-        }*/
+            
+            int length_humans = listHumans.Count();
+		double[] age_weight_height = new double[length_humans];
+            int[] count_hum = new int[length_humans];
+            for(int n = 0; n < length_humans; n++)
+            {
+                count_hum[n] = n;
+            if (choise == 1){age_weight_height[n] = listHumans[n].Age;}
+            else if(choise == 2){age_weight_height[n] = listHumans[n].Weight;}
+            else if(choise == 3){age_weight_height[n] = listHumans[n].Height;}
+                
+            }
+		    int j = length_humans - 1;
+		    for (int b = 0; b < j; b++) {
+			    for (int i = j; i != 0; i--) {
+				    if (age_weight_height[i] < age_weight_height[i - 1]) {
+					    double temp = age_weight_height[i];
+					    age_weight_height[i] = age_weight_height[i - 1];
+					    age_weight_height[i - 1] = temp;
+                        int temp_count = count_hum[i];
+					    count_hum[i] = count_hum[i - 1];
+					    count_hum[i - 1] = temp_count;
+				     }
+			    }
+		    }
+            if (listHumans.Count() != 0) {
+               Console.WriteLine("Список отсортирован");
+               for(int k = listHumans.Count()-1 ; k >= 0; k--)
+                {
+                    listHumans[count_hum[k]].printInfo();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Список пустой");
+            }
+        }
         public void average_age()
         {
             int age = 0;
